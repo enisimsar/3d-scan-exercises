ac44ce6302026ac83ed0256d49810eeb43440433
Enis Simsar <enisimsar@gmail.com>
2019-10-27 19:42:09 +0100

    Add invalid vertices as well

--- exercise_1/main.cpp
+++ exercise_1/main.cpp
@@ -8,37 +8,37 @@
 
 struct Vertex
 {
-	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-	
-	// position stored as 4 floats (4th component is supposed to be 1.0)
-	Vector4f position;
-	// color stored as 4 unsigned char
-	Vector4uc color;
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    // position stored as 4 floats (4th component is supposed to be 1.0)
+    Vector4f position;
+    // color stored as 4 unsigned char
+    Vector4uc color;
 };
 
 bool WriteMesh(Vertex* vertices, unsigned int width, unsigned int height, const std::string& filename)
 {
-	float edgeThreshold = 0.01f; // 1cm
+    float edgeThreshold = 0.01f; // 1cm
 
-	// TODO 2: use the OFF file format to save the vertices grid (http://www.geomview.org/docs/html/OFF.html)
-	// - have a look at the "off_sample.off" file to see how to store the vertices and triangles
-	// - for debugging we recommend to first only write out the vertices (set the number of faces to zero)
-	// - for simplicity write every vertex to file, even if it is not valid (position.x() == MINF) (note that all vertices in the off file have to be valid, thus, if a point is not valid write out a dummy point like (0,0,0))
-	// - use a simple triangulation exploiting the grid structure (neighboring vertices build a triangle, two triangles per grid cell)
-	// - you can use an arbitrary triangulation of the cells, but make sure that the triangles are consistently oriented
-	// - only write triangles with valid vertices and an edge length smaller then edgeThreshold
+    // TODO 2: use the OFF file format to save the vertices grid (http://www.geomview.org/docs/html/OFF.html)
+    // - have a look at the "off_sample.off" file to see how to store the vertices and triangles
+    // - for debugging we recommend to first only write out the vertices (set the number of faces to zero)
+    // - for simplicity write every vertex to file, even if it is not valid (position.x() == MINF) (note that all vertices in the off file have to be valid, thus, if a point is not valid write out a dummy point like (0,0,0))
+    // - use a simple triangulation exploiting the grid structure (neighboring vertices build a triangle, two triangles per grid cell)
+    // - you can use an arbitrary triangulation of the cells, but make sure that the triangles are consistently oriented
+    // - only write triangles with valid vertices and an edge length smaller then edgeThreshold
 
-	// TODO: Get number of vertices
-	unsigned int nVertices = 0;
+    // TODO: Get number of vertices
+    unsigned int nVertices = width * height;
+//
+//    for (unsigned int i = 0; i < width*height; ++i)
+//    {
+//        Vertex v = vertices[i];
+//        if (v.position.x() != MINF) nVertices++;
+//    }
 
-    for (unsigned int i = 0; i < width*height; ++i)
-    {
-        Vertex v = vertices[i];
-        if (v.position.x() != MINF) nVertices++;
-    }
-
-	// TODO: Determine number of valid faces
-	unsigned nFaces = 0;
+    // TODO: Determine number of valid faces
+    unsigned nFaces =0;// width * height / 2;
 
     for (unsigned int i = 0; i < width*height; ++i)
     {
@@ -65,15 +65,15 @@ bool WriteMesh(Vertex* vertices, unsigned int width, unsigned int height, const 
         if (face2) nFaces++;
     }
 
-	// Write off file
-	std::ofstream outFile(filename);
-	if (!outFile.is_open()) return false;
+    // Write off file
+    std::ofstream outFile(filename);
+    if (!outFile.is_open()) return false;
 
-	// write header
-	outFile << "COFF" << std::endl;
-	outFile << nVertices << " " << nFaces << " 0" << std::endl;
+    // write header
+    outFile << "COFF" << std::endl;
+    outFile << nVertices << " " << nFaces << " 0" << std::endl;
 
-	// TODO: save vertices
+    // TODO: save vertices
     for (unsigned int i = 0; i < width*height; ++i)
     {
         Vertex v = vertices[i];
@@ -81,12 +81,15 @@ bool WriteMesh(Vertex* vertices, unsigned int width, unsigned int height, const 
             // X Y Z R G B A
             outFile << v.position.x() << " " << v.position.y() << " " << v.position.z() << " ";
             outFile << (int) v.color.x() << " " << (int) v.color.y() << " ";
-            outFile << (int) v.color.z() << " " << (int) v.color.w() << std::endl;
+            outFile << (int) v.color.z() << " " << "255" << std::endl;
+        }
+        else {
+            outFile << "0 0 0 0 0 0 0" << std::endl;
         }
     }
-	
 
-	// TODO: save valid faces
+
+    // TODO: save valid faces
 
     for (unsigned int i = 0; i < width*height; ++i)
     {
@@ -114,57 +117,57 @@ bool WriteMesh(Vertex* vertices, unsigned int width, unsigned int height, const 
     }
 
 
-	// close file
-	outFile.close();
+    // close file
+    outFile.close();
 
-	return true;
+    return true;
 }
 
 int main()
 {
-	// Make sure this path points to the data folder
-	std::string filenameIn = "../data/rgbd_dataset_freiburg1_xyz/";
-	std::string filenameBaseOut = "mesh_";
+    // Make sure this path points to the data folder
+    std::string filenameIn = "../data/rgbd_dataset_freiburg1_xyz/";
+    std::string filenameBaseOut = "mesh_";
 
-	// load video
-	std::cout << "Initialize virtual sensor..." << std::endl;
-	VirtualSensor sensor;
-	if (!sensor.Init(filenameIn))
-	{
-		std::cout << "Failed to initialize the sensor!\nCheck file path!" << std::endl;
-		return -1;
-	}
+    // load video
+    std::cout << "Initialize virtual sensor..." << std::endl;
+    VirtualSensor sensor;
+    if (!sensor.Init(filenameIn))
+    {
+        std::cout << "Failed to initialize the sensor!\nCheck file path!" << std::endl;
+        return -1;
+    }
 
-	// convert video to meshes
-	while (sensor.ProcessNextFrame())
-	{
-		// get ptr to the current depth frame
-		// depth is stored in row major (get dimensions via sensor.GetDepthImageWidth() / GetDepthImageHeight())
-		float* depthMap = sensor.GetDepth();
-		// get ptr to the current color frame
-		// color is stored as RGBX in row major (4 byte values per pixel, get dimensions via sensor.GetColorImageWidth() / GetColorImageHeight())
-		BYTE* colorMap = sensor.GetColorRGBX();
+    // convert video to meshes
+    while (sensor.ProcessNextFrame())
+    {
+        // get ptr to the current depth frame
+        // depth is stored in row major (get dimensions via sensor.GetDepthImageWidth() / GetDepthImageHeight())
+        float* depthMap = sensor.GetDepth();
+        // get ptr to the current color frame
+        // color is stored as RGBX in row major (4 byte values per pixel, get dimensions via sensor.GetColorImageWidth() / GetColorImageHeight())
+        BYTE* colorMap = sensor.GetColorRGBX();
 
-		// get depth intrinsics
-		Matrix3f depthIntrinsics = sensor.GetDepthIntrinsics();
-		float fX = depthIntrinsics(0, 0);
-		float fY = depthIntrinsics(1, 1);
-		float cX = depthIntrinsics(0, 2);
-		float cY = depthIntrinsics(1, 2);
+        // get depth intrinsics
+        Matrix3f depthIntrinsics = sensor.GetDepthIntrinsics();
+        float fX = depthIntrinsics(0, 0);
+        float fY = depthIntrinsics(1, 1);
+        float cX = depthIntrinsics(0, 2);
+        float cY = depthIntrinsics(1, 2);
 
-		// compute inverse depth extrinsics
-		Matrix4f depthExtrinsicsInv = sensor.GetDepthExtrinsics().inverse();
+        // compute inverse depth extrinsics
+        Matrix4f depthExtrinsicsInv = sensor.GetDepthExtrinsics().inverse();
 
-		Matrix4f trajectory = sensor.GetTrajectory();
-		Matrix4f trajectoryInv = sensor.GetTrajectory().inverse();
+        Matrix4f trajectory = sensor.GetTrajectory();
+        Matrix4f trajectoryInv = sensor.GetTrajectory().inverse();
 
-		// TODO 1: back-projection
-		// write result to the vertices array below, keep pixel ordering!
-		// if the depth value at idx is invalid (MINF) write the following values to the vertices array
-		// vertices[idx].position = Vector4f(MINF, MINF, MINF, MINF);
-		// vertices[idx].color = Vector4uc(0,0,0,0);
-		// otherwise apply back-projection and transform the vertex to world space, use the corresponding color from the colormap
-		Vertex* vertices = new Vertex[sensor.GetDepthImageWidth() * sensor.GetDepthImageHeight()];
+        // TODO 1: back-projection
+        // write result to the vertices array below, keep pixel ordering!
+        // if the depth value at idx is invalid (MINF) write the following values to the vertices array
+        // vertices[idx].position = Vector4f(MINF, MINF, MINF, MINF);
+        // vertices[idx].color = Vector4uc(0,0,0,0);
+        // otherwise apply back-projection and transform the vertex to world space, use the corresponding color from the colormap
+        Vertex* vertices = new Vertex[sensor.GetDepthImageWidth() * sensor.GetDepthImageHeight()];
 
         for (unsigned int idx = 0; idx < sensor.GetDepthImageWidth() * sensor.GetDepthImageHeight(); ++idx)
         {
@@ -192,20 +195,18 @@ int main()
         }
 
 
-		// write mesh file
-		std::stringstream ss;
-		ss << filenameBaseOut << sensor.GetCurrentFrameCnt() << ".off";
-		if (!WriteMesh(vertices, sensor.GetDepthImageWidth(), sensor.GetDepthImageHeight(), ss.str()))
-		{
-			std::cout << "Failed to write mesh!\nCheck file path!" << std::endl;
-			return -1;
-		}
+        // write mesh file
+        std::stringstream ss;
+        ss << filenameBaseOut << sensor.GetCurrentFrameCnt() << ".off";
+        if (!WriteMesh(vertices, sensor.GetDepthImageWidth(), sensor.GetDepthImageHeight(), ss.str()))
+        {
+            std::cout << "Failed to write mesh!\nCheck file path!" << std::endl;
+            return -1;
+        }
 
-		// free mem
-		delete[] vertices;
+        // free mem
+        delete[] vertices;
+    }
 
-		return 0;
-	}
-
-	return 0;
+    return 0;
 }
